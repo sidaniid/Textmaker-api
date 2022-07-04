cb07a2d8c23c7ac1fb4606951a7dca94d25e47bf
Muhamad Ramdani <59561245+sidaniid@users.noreply.github.com>
2022-07-04 07:29:28 +0000

    tired.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
 exports.textpro = require("../lib/textpro");
 exports.photooxy = require("../lib/photooxy");
-exports.photooxy = require("../lib/ephoto");
+exports.ephoto = require("../lib/ephoto");
